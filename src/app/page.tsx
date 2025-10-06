dfe851035baf94fb249c585c1f7cfede1960cbda
ASMIT_1206 <asmit12yadav@gmail.com>
2025-10-06 22:26:42 +0530

    Add TestimonialCards and UpcomingWebinars components; implement scroll animation in globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import { main } from "framer-motion/client";
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import StickyScrool from "@/components/StickyScrool";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 export default function Home() {
   return (
   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection/>
     <FeaturedCourses/>
     <StickyScrool/>
+    <TestimonialCards/>
+    <UpcomingWebinars/>
   </main>
   );
 }
